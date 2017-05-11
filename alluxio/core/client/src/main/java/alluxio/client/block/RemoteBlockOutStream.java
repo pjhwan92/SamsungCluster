e077f1cda50afd7977d39a6f648062728598f9e6
pjh <pjhwan92@github.com>
2017-05-11 19:26:52 +0900

    alluxio added

---
+++ alluxio/core/client/src/main/java/alluxio/client/block/RemoteBlockOutStream.java
@@ -0,0 +1,136 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.client.block;
+
+import alluxio.client.RemoteBlockWriter;
+import alluxio.exception.AlluxioException;
+import alluxio.metrics.MetricsSystem;
+import alluxio.wire.WorkerNetAddress;
+
+import com.codahale.metrics.Counter;
+import com.google.common.io.Closer;
+
+import java.io.IOException;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * Provides a streaming API to write to an Alluxio block. This output stream will send the write
+ * through an Alluxio worker which will then write the block to a file in Alluxio storage.
+ */
+@NotThreadSafe
+public final class RemoteBlockOutStream extends BufferedBlockOutStream {
+  private final RemoteBlockWriter mRemoteWriter;
+  private final BlockWorkerClient mBlockWorkerClient;
+  private final Closer mCloser;
+
+  /**
+   * Creates a new block output stream on a specific address.
+   *
+   * @param blockId the block id
+   * @param blockSize the block size
+   * @param address the address of the preferred worker
+   * @param blockStoreContext the block store context
+   * @throws IOException if I/O error occurs
+   */
+  public RemoteBlockOutStream(long blockId,
+      long blockSize,
+      WorkerNetAddress address,
+      BlockStoreContext blockStoreContext) throws IOException {
+    super(blockId, blockSize, blockStoreContext);
+    mCloser = Closer.create();
+    try {
+      mRemoteWriter = mCloser.register(RemoteBlockWriter.Factory.create());
+      mBlockWorkerClient = mCloser.register(mContext.createWorkerClient(address));
+
+      mRemoteWriter.open(mBlockWorkerClient.getDataServerAddress(), mBlockId,
+          mBlockWorkerClient.getSessionId());
+    } catch (IOException e) {
+      mCloser.close();
+      throw e;
+    }
+  }
+
+  @Override
+  public void cancel() throws IOException {
+    if (mClosed) {
+      return;
+    }
+    try {
+      mBlockWorkerClient.cancelBlock(mBlockId);
+    } catch (AlluxioException e) {
+      throw new IOException(e);
+    } finally {
+      mClosed = true;
+      mCloser.close();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (mClosed) {
+      return;
+    }
+
+    try {
+      flush();
+      if (mFlushedBytes > 0) {
+        try {
+          mBlockWorkerClient.cacheBlock(mBlockId);
+        } catch (AlluxioException e) {
+          throw new IOException(e);
+        }
+        Metrics.BLOCKS_WRITTEN_REMOTE.inc();
+      } else {
+        try {
+          mBlockWorkerClient.cancelBlock(mBlockId);
+        } catch (AlluxioException e) {
+          throw new IOException(e);
+        }
+      }
+    } finally {
+      mClosed = true;
+      mCloser.close();
+    }
+  }
+
+  @Override
+  public void flush() throws IOException {
+    writeToRemoteBlock(mBuffer.array(), 0, mBuffer.position());
+    mBuffer.clear();
+  }
+
+  @Override
+  protected void unBufferedWrite(byte[] b, int off, int len) throws IOException {
+    writeToRemoteBlock(b, off, len);
+  }
+
+  private void writeToRemoteBlock(byte[] b, int off, int len) throws IOException {
+    mRemoteWriter.write(b, off, len);
+    mFlushedBytes += len;
+    Metrics.BYTES_WRITTEN_REMOTE.inc(len);
+  }
+
+  /**
+   * Class that contains metrics about RemoteBlockOutStream.
+   */
+  @ThreadSafe
+  private static final class Metrics {
+    private static final Counter BLOCKS_WRITTEN_REMOTE =
+        MetricsSystem.clientCounter("BlocksWrittenRemote");
+    private static final Counter BYTES_WRITTEN_REMOTE =
+        MetricsSystem.clientCounter("BytesWrittenRemote");
+
+    private Metrics() {} // prevent instantiation
+  }
+}
---
+++ alluxio/core/client/src/test/java/alluxio/client/file/options/CreateFileOptionsTest.java
@@ -0,0 +1,105 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.client.file.options;
+
+import alluxio.CommonTestUtils;
+import alluxio.Configuration;
+import alluxio.Constants;
+import alluxio.PropertyKey;
+import alluxio.client.WriteType;
+import alluxio.client.file.policy.FileWriteLocationPolicy;
+import alluxio.client.file.policy.RoundRobinPolicy;
+import alluxio.security.authorization.Mode;
+import alluxio.thrift.CreateFileTOptions;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Random;
+
+/**
+ * Tests for the {@link CreateFileOptions} class.
+ */
+@RunWith(PowerMockRunner.class)
+// Need to mock Mode to use CommonTestUtils#testEquals.
+@PrepareForTest(Mode.class)
+public class CreateFileOptionsTest {
+  private final long mDefaultBlockSizeBytes = Configuration.getBytes(
+      PropertyKey.USER_BLOCK_SIZE_BYTES_DEFAULT);
+  private final WriteType mDefaultWriteType = Configuration.getEnum(
+      PropertyKey.USER_FILE_WRITE_TYPE_DEFAULT, alluxio.client.WriteType.class);
+
+  // TODO(calvin): Test location policy when a factory is created
+  @Test
+  public void defaults() {
+    CreateFileOptions options = CreateFileOptions.defaults();
+    Assert.assertTrue(options.isRecursive());
+    Assert.assertEquals(mDefaultBlockSizeBytes, options.getBlockSizeBytes());
+    Assert.assertEquals(mDefaultWriteType.getAlluxioStorageType(), options.getAlluxioStorageType());
+    Assert.assertEquals(mDefaultWriteType.getUnderStorageType(), options.getUnderStorageType());
+    Assert.assertEquals(Constants.NO_TTL, options.getTtl());
+  }
+
+  /**
+   * Tests getting and setting fields.
+   */
+  @Test
+  public void fields() {
+    Random random = new Random();
+    long blockSize = random.nextLong();
+    FileWriteLocationPolicy policy = new RoundRobinPolicy();
+    Mode mode = new Mode((short) 0123);
+    boolean recursive = random.nextBoolean();
+    long ttl = random.nextLong();
+    WriteType writeType = WriteType.NONE;
+
+    CreateFileOptions options = CreateFileOptions.defaults();
+    options.setBlockSizeBytes(blockSize);
+    options.setLocationPolicy(policy);
+    options.setMode(mode);
+    options.setRecursive(recursive);
+    options.setTtl(ttl);
+    options.setWriteType(writeType);
+
+    Assert.assertEquals(blockSize, options.getBlockSizeBytes());
+    Assert.assertEquals(policy, options.getLocationPolicy());
+    Assert.assertEquals(mode, options.getMode());
+    Assert.assertEquals(recursive, options.isRecursive());
+    Assert.assertEquals(ttl, options.getTtl());
+    Assert.assertEquals(writeType.getAlluxioStorageType(), options.getAlluxioStorageType());
+    Assert.assertEquals(writeType.getUnderStorageType(), options.getUnderStorageType());
+  }
+
+  /**
+   * Tests conversion to thrift representation.
+   */
+  @Test
+  public void toThrift() {
+    CreateFileOptions options = CreateFileOptions.defaults();
+    CreateFileTOptions thriftOptions = options.toThrift();
+    Assert.assertTrue(thriftOptions.isRecursive());
+    Assert.assertTrue(thriftOptions.isSetPersisted());
+    Assert.assertEquals(mDefaultWriteType.getUnderStorageType().isSyncPersist(), thriftOptions
+        .isPersisted());
+    Assert.assertEquals(mDefaultBlockSizeBytes, thriftOptions.getBlockSizeBytes());
+    Assert.assertEquals(Constants.NO_TTL, thriftOptions.getTtl());
+    Assert.assertFalse(thriftOptions.isSetMode());
+  }
+
+  @Test
+  public void equalsTest() throws Exception {
+    CommonTestUtils.testEquals(CreateFileOptions.class);
+  }
+}
