9c32bbd71d37a08e8aa261e51b592424e7e7723f
pjh <pjhwan92@github.com>
2017-05-14 14:21:04 +0900

    example app

---
+++ apps/cache_wc/build.sbt
@@ -0,0 +1,7 @@
+name := "cache_wc"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
---
+++ apps/cache_wc/src/main/scala/kr/edu/skku/arcs/wc/WordCount.scala
@@ -0,0 +1,43 @@
+package kr.edu.skku.arcs.wc
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+
+/**
+  * Created by pjh on 5/12/17.
+  */
+class WordCount {
+  var sc: SparkContext = null
+
+  def WordCount () {
+    val hdfs = "hdfs://compute11:9000/user/arcs/"
+    val alluxio = "hdfs://compute11:19998/"
+
+    val conf = new SparkConf ().setAppName ("word count");
+    var textFile: RDD[String] = null
+
+    sc = new SparkContext (conf)
+
+    textFile = sc.textFile (hdfs + "kdda")
+    execute (textFile)
+    execute (textFile)
+
+    textFile = sc.textFile (alluxio + "kdda")
+    execute (textFile)
+    execute (textFile)
+
+    textFile = sc.textFile (hdfs + "kddb")
+    execute (textFile)
+    execute (textFile)
+
+    textFile = sc.textFile (alluxio + "kddb")
+    execute (textFile)
+    execute (textFile)
+  }
+
+  def execute (input: RDD[String]): Unit = {
+    input.flatMap (line => line.split (" "))
+         .map (word => (word, 1))
+         .reduceByKey {case (x, y) => x + y}
+  }
+}
