d25e6fbd5e649cfeec2e4e3e3d11e20fc15ca968
pjh <pjhwan92@github.com>
2017-05-15 16:09:51 +0900

    example app

--- apps/cache_wc/src/main/scala/kr/edu/skku/arcs/wc/WordCount.scala
+++ apps/cache_wc/src/main/scala/kr/edu/skku/arcs/wc/WordCount.scala
@@ -13,6 +13,7 @@ class WordCount {
   def wc () {
     val hdfs = "hdfs://compute11:9000/user/arcs/"
     val alluxio = "alluxio://compute11:19998/"
+    val files = List ("_2500", "_5000", "_7500", "_10000")
 
     //val conf = new SparkConf ().setAppName ("word count");
     val spark = SparkSession.builder.appName ("word count").getOrCreate ()
@@ -20,69 +21,42 @@ class WordCount {
     var cachedRdd: RDD[String] = null
 
     sc = spark.sparkContext
-    textFile = sc.textFile (hdfs + "kdda")
+    textFile = sc.textFile (hdfs + "")
     textFile.count()
 
-    // hdfs kdda
-    textFile = sc.textFile (hdfs + "kdda")
-    textFile.flatMap(_.split(" "))
-      .map((_,1))
-      .reduceByKey(_+_)
-      .saveAsTextFile(hdfs + "wc_hdfs_kdda_result_1")           // 0
-    cachedRdd = sc.textFile (hdfs + "kdda").cache()
-    cachedRdd.flatMap(_.split(" "))
-      .map ((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_hdfs_kdda_result_2")          // 1
-    cachedRdd.flatMap(_.split(" "))
-      .map ((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_hdfs_kdda_result_3")          // 2
+    for (file <- files) {
+      // hdfs
+      textFile = sc.textFile(hdfs + file)
+      textFile.flatMap(_.split(" "))
+        .map((_, 1))
+        .reduceByKey(_ + _)
+        .saveAsTextFile(hdfs + "wc_hdfs" + file + "_result_1") // 0
+      cachedRdd = sc.textFile(hdfs + file).cache()
+      cachedRdd.flatMap(_.split(" "))
+        .map((_, 1))
+        .reduceByKey(_ + _)
+        .saveAsTextFile(hdfs + "wc_hdfs" + file + "_result_2") // 1
+      cachedRdd.flatMap(_.split(" "))
+        .map((_, 1))
+        .reduceByKey(_ + _)
+        .saveAsTextFile(hdfs + "wc_hdfs" + file + "_result_3") // 2
 
-    // alluxio kdda
-    textFile = sc.textFile (hdfs + "kdda")
-    textFile.flatMap (_.split (" "))
-      .map((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_alluxio_kdda_result_1")    // 3
-    sc.textFile (hdfs + "kdda").saveAsTextFile (alluxio + "wc_kdda_med_result_1")
-    cachedRdd = sc.textFile (alluxio + "wc_kdda_med_result_1")
-    cachedRdd.flatMap (_.split (" " ))
-      .map ((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_alluxio_kdda_result_2")    // 5
+      // alluxio
+      textFile = sc.textFile(hdfs + file)
+      textFile.flatMap(_.split(" "))
+        .map((_, 1))
+        .reduceByKey(_ + _)
+        .saveAsTextFile(hdfs + "wc_alluxio" + file + "_result_1") // 3
+      sc.textFile(hdfs + file).saveAsTextFile(alluxio + "wc" + file + "_med_result_1")
+      cachedRdd = sc.textFile(alluxio + "wc" + file + "_med_result_1")
+      cachedRdd.flatMap(_.split(" "))
+        .map((_, 1))
+        .reduceByKey(_ + _)
+        .saveAsTextFile(hdfs + "wc_alluxio" + file + "_result_2") // 5
 
-    textFile = sc.textFile (hdfs + "kddb")
-    textFile.count()
-
-    // hdfs kddb
-    textFile = sc.textFile (hdfs + "kddb")
-    textFile.flatMap(_.split(" "))
-      .map((_,1))
-      .reduceByKey(_+_)
-      .saveAsTextFile(hdfs + "wc_hdfs_kddb_result_1")           // 6
-    cachedRdd = sc.textFile (hdfs + "kddb").cache()
-    cachedRdd.flatMap (_.split (" "))
-      .map ((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_hdfs_kddb_result_2")          // 7
-    cachedRdd.flatMap (_.split (" "))
-      .map ((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_hdfs_kddb_result_3")          // 8
-
-    // alluxio kddb
-    textFile = sc.textFile (hdfs + "kddb")
-    textFile.flatMap (_.split (" "))
-      .map((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_alluxio_kddb_result_1")    // 9
-    sc.textFile (hdfs + "kdda").saveAsTextFile (alluxio + "wc_kddb_med_result_1")
-    cachedRdd = sc.textFile (alluxio + "wc_kddb_med_result_1")
-    cachedRdd.flatMap (_.split (" " ))
-      .map ((_, 1))
-      .reduceByKey (_ + _)
-      .saveAsTextFile (hdfs + "wc_alluxio_kddb_result_2")    // 5
+      textFile = sc.textFile(hdfs + file)
+      textFile.count()
+    }
   }
 
   def wc2 () {
