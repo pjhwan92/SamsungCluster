697e458a343753cc3ebbe9a9153d081b58de645f
pjh <pjhwan92@github.com>
2017-07-19 14:26:45 +0900

    shell bug fix

--- alluxio/shell/src/main/java/alluxio/shell/command/PrefetchCommand.java
+++ alluxio/shell/src/main/java/alluxio/shell/command/PrefetchCommand.java
@@ -65,7 +65,7 @@ public final class PrefetchCommand extends AbstractShellCommand {
 
   @Override
   protected Options getOptions() {
-    return null;
+    return new Options ();
   }
 
   @Override
@@ -105,143 +105,13 @@ public final class PrefetchCommand extends AbstractShellCommand {
     }
   }
 
-  /**
-   * Copies a list of files or directories specified by srcPaths to the destination specified by
-   * dstPath. This method is used when the original source path contains wildcards.
-   *
-   * @param srcPaths a list of files or directories in the Alluxio filesystem
-   * @param dstPath the destination in the Alluxio filesystem
-   * @param recursive indicates whether directories should be copied recursively
-   * @throws AlluxioException when Alluxio exception occurs
-   * @throws IOException when non-Alluxio exception occurs
-   */
-  private void copyWildcard(List<AlluxioURI> srcPaths, AlluxioURI dstPath, boolean recursive)
-      throws AlluxioException, IOException {
-    URIStatus dstStatus = null;
-    try {
-      dstStatus = mFileSystem.getStatus(dstPath);
-    } catch (FileDoesNotExistException e) {
-      // if the destination does not exist, it will be created
-    }
-
-    if (dstStatus != null && !dstStatus.isFolder()) {
-      throw new InvalidPathException(ExceptionMessage.DESTINATION_CANNOT_BE_FILE.getMessage());
-    }
-    if (dstStatus == null) {
-      mFileSystem.createDirectory(dstPath);
-      System.out.println("Created directory: " + dstPath.getPath());
-    }
-    List<String> errorMessages = new ArrayList<>();
-    for (AlluxioURI srcPath : srcPaths) {
-      try {
-        copy(srcPath, new AlluxioURI(dstPath.getScheme(), dstPath.getAuthority(),
-            PathUtils.concatPath(dstPath.getPath(), srcPath.getName())), recursive);
-      } catch (AlluxioException | IOException e) {
-        errorMessages.add(e.getMessage());
-      }
-    }
-    if (errorMessages.size() != 0) {
-      throw new IOException(Joiner.on('\n').join(errorMessages));
-    }
-  }
-
-  /**
-   * Copies a file or a directory in the Alluxio filesystem.
-   *
-   * @param srcPath the source {@link AlluxioURI} (could be a file or a directory)
-   * @param dstPath the destination path in the Alluxio filesystem
-   * @param recursive indicates whether directories should be copied recursively
-   * @throws AlluxioException when Alluxio exception occurs
-   * @throws IOException when non-Alluxio exception occurs
-   */
-  private void copy(AlluxioURI srcPath, AlluxioURI dstPath, boolean recursive)
-      throws AlluxioException, IOException {
-    URIStatus srcStatus = mFileSystem.getStatus(srcPath);
-
-    URIStatus dstStatus = null;
-    try {
-      dstStatus = mFileSystem.getStatus(dstPath);
-    } catch (FileDoesNotExistException e) {
-      // if the destination does not exist, it will be created
-    }
-
-    if (!srcStatus.isFolder()) {
-      if (dstStatus != null && dstStatus.isFolder()) {
-        dstPath = new AlluxioURI(PathUtils.concatPath(dstPath.getPath(), srcPath.getName()));
-      }
-      copyFile(srcPath, dstPath);
-    } else {
-      if (!recursive) {
-        throw new IOException(
-            srcPath.getPath() + " is a directory, to copy it please use \"cp -R <src> <dst>\"");
-      }
-
-      List<URIStatus> statuses;
-      statuses = mFileSystem.listStatus(srcPath);
-
-      if (dstStatus != null) {
-        if (!dstStatus.isFolder()) {
-          throw new InvalidPathException(ExceptionMessage.DESTINATION_CANNOT_BE_FILE.getMessage());
-        }
-        // if copying a directory to an existing directory, the copied directory will become a
-        // subdirectory of the destination
-        if (srcStatus.isFolder()) {
-          dstPath = new AlluxioURI(PathUtils.concatPath(dstPath.getPath(), srcPath.getName()));
-          mFileSystem.createDirectory(dstPath);
-          System.out.println("Created directory: " + dstPath.getPath());
-        }
-      }
-
-      if (dstStatus == null) {
-        mFileSystem.createDirectory(dstPath);
-        System.out.println("Created directory: " + dstPath.getPath());
-      }
-
-      List<String> errorMessages = new ArrayList<>();
-      for (URIStatus status : statuses) {
-        try {
-          copy(new AlluxioURI(srcPath.getScheme(), srcPath.getAuthority(), status.getPath()),
-              new AlluxioURI(dstPath.getScheme(), dstPath.getAuthority(),
-                  PathUtils.concatPath(dstPath.getPath(), status.getName())), recursive);
-        } catch (IOException e) {
-          errorMessages.add(e.getMessage());
-        }
-      }
-
-      if (errorMessages.size() != 0) {
-        throw new IOException(Joiner.on('\n').join(errorMessages));
-      }
-    }
-  }
-
-  /**
-   * Copies a file in the Alluxio filesystem.
-   *
-   * @param srcPath the source {@link AlluxioURI} (has to be a file)
-   * @param dstPath the destination path in the Alluxio filesystem
-   * @throws AlluxioException when Alluxio exception occurs
-   * @throws IOException when non-Alluxio exception occurs
-   */
-  private void copyFile(AlluxioURI srcPath, AlluxioURI dstPath)
-      throws AlluxioException, IOException {
-    try (Closer closer = Closer.create()) {
-      OpenFileOptions openFileOptions = OpenFileOptions.defaults().setReadType(ReadType.NO_CACHE);
-      FileInStream is = closer.register(mFileSystem.openFile(srcPath, openFileOptions));
-      CreateFileOptions createFileOptions = CreateFileOptions.defaults();
-      FileOutStream os = closer.register(mFileSystem.createFile(dstPath, createFileOptions));
-      IOUtils.copy(is, os);
-      System.out.println("Copied " + srcPath + " to " + dstPath.getPath());
-    }
-  }
-
   @Override
   public String getUsage() {
-    return "cp [-R] <src> <dst>";
+    return "prefetch <path>";
   }
 
   @Override
   public String getDescription() {
-    return "Copies a file or a directory in the Alluxio filesystem. "
-        + "The -R flag is needed to copy directories.";
+    return "Simply prefetch files into target workers";
   }
 }
